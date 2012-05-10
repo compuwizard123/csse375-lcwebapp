700d4faa9c175b143641a8990831f4fe308e1683
Kevin Risden <risdenkj@rose-hulman.edu>
2012-05-10 10:41:06 -0400

    updated rest handler

--- rest_handler.php
+++ rest_handler.php
@@ -7,31 +7,49 @@
 	header('Content-type: application/json');
 	
 	switch(str_replace('/rest/', '', $_SERVER['REQUEST_URI']))
-	{			
+	{
+		case "add_course_for_tutor":
+			$result = add_course_for_tutor($args["LCTutorID"],$args["LCCourseID"]);
+			break;
+		case "remove_course_for_tutor":
+			$result = remove_course_for_tutor($args["LCTutorID"],$args["LCCourseID"]);
+			break;
+
+		case "add_tutor":
+			$result = add_tutor($args["LCTutorName"],$args["LCTutorYear"],$args["LCTutorID"],$args["LCTutorMajor"],$args["LCTutorRoomNumber"],$args["LCTutorBio"], $args["LCCoursesArray"]);
+			break;
+		case "delete_tutor":
+			$result = delete_tutor($args["LCTutorID"]);
+			break;
+
+		case "get_course_by_crn":
+			$result = get_course_by_crn($args['LCCourseNumber']);
+			break;
+
 		case "get_tutor_by_id":
 			$result = get_tutor_by_id($args['LCTutorID']);
 			break;
 			
 		case "get_tutors_by_name":
-			$result = get_tutors_by_name($args["LCTutorName"]);
+			$result = get_tutors_by_name($args["LCTutorName"],$args["LCCourseNumber"]);
 			break;
-			
+
 		case "check_if_booked":
 			$result = check_if_booked($args["LCTutorID"],$args["LCTimeslotID"],$args["LCDate"]);
 			break;
 			
 		case "get_tutors_by_course":
-			$result = get_tutors_by_course($args["LCCourse"]);
+			$result = get_tutors_by_course($args["LCCourseNumber"]);
 			break; 
-			
+
 		case "get_tutor_booked_timeslots":
 			$result = get_tutor_booked_timeslots($args['LCTutorID'], $args['LCDate']);
 			break;
-			
+
 		case "get_tutor_courses_tutored":
 			$result = get_tutor_courses_tutored($args['LCTutorID']);
 			break;
-			
+
 		case "book_timeslot":
 			$result = book_timeslot($args['LCTutorID'], $args['LCTuteeUname'],$args['LCTimeslotID'],$args['LCDate']);
 			break;
